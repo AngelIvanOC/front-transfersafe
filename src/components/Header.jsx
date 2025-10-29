af2dfc31af074a9104435ae23e289b56ee62e702
AngelIvanOC <168600662+AngelIvanOC@users.noreply.github.com>
2025-10-29 11:34:38 -0600

    Yaaa

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -18,7 +18,7 @@ function Header() {
         <div className="nav-left">
           <div className="logo" onClick={scrollToTop}>
             <img
-              src="/src/assets/Logo.png"
+              src="/src/public/Logo.png"
               alt="TransferSafe"
               className="logo-img"
             />
